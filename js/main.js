401cd4f96e25adc5d15b839a00fb2727b859c20f
raihanfswd <63357401+raihanfswd@users.noreply.github.com>
2020-07-14 00:15:33 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function ($) {
+  
+   const ps = new PerfectScrollbar('.testimonial_author_name', {
+  wheelSpeed: 2,
+  wheelPropagation: true,
+  minScrollbarLength: 5,
+});
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+//  $(".listing_carousel").owlCarousel({
+//    items: 3,
+//    loop: true,
+//    nav: true,
+//    dots: true,
+//    autoplay: false,
+//    navText: ['<i class="fa fa-long-arrow-left"> </i>', '<i class="fa fa-long-arrow-right"> </i>'],
+//    margin: 30,
+//    center: true,
+//    responsiveClass: true,
+//    responsive: {
+//      0: {
+//        items: 1,
+//        dots: true,
+//      },
+//      600: {
+//        items: 2,
+//        dots: true,
+//      },
+//      1000: {
+//        items: 3,
+//        dots: true,
+//      }
+//    }
+//  });
+
+});
